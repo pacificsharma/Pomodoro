2434a25094c77d08bd2f3224dd02f4b6c22e6f1a
Prashant Sharma <pacificsharmma@gmail.com>
2018-08-12 10:28:26 +0530

    Implemented Routing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Header from "./containers/header";
+import Main from "./containers/main";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-class HelloWorld extends React.Component {
+class App extends React.Component {
   render() {
-    return <h1>Hello World!!!</h1>;
+    return (<div>
+      <Header />
+      <Main />
+    </div>);
   }
 }
 
 var app = document.getElementById('app');
-ReactDOM.render(<HelloWorld />, app);
-
-// console.log('hello');
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('app'));
---
+++ src/containers/help.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Help extends React.Component {
+    render() {
+        return <div>
+            <h1>Welcome to Help Page</h1>
+        </div >
+    }
+}
+
+export default Help
